4d4dacfdf9710cf1d9ddf7d6a62e3147b4fc15f0
Vlad <40580001+Javist01@users.noreply.github.com>
2022-11-24 21:37:44 +0200

    /

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App/App';
 import './index.css';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from 'components/constants/Theme/Theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
